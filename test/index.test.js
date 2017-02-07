75e384b415acbd1bef7010887942d6b21a774abf
Kiril Vatev <vatev.1@gmail.com>
2017-02-06 21:14:47 -0500

    adding some tests

---
+++ test/index.test.js
@@ -0,0 +1,32 @@
+/* jshint node: true, mocha: true */
+
+var stream = require('stream');
+
+var expect = require('chai').expect;
+var through = require('through');
+
+var lib = require('../');
+
+describe('[index]', function () {
+  it('is a function', function () {
+    expect(lib).to.be.a('function');
+  });
+
+  it('returns a stream', function () {
+    var out = lib();
+
+    // test that it is stream-like...
+    expect(out).to.be.instanceof(stream);
+    expect(out).to.have.property('pipe').and.to.be.a('function');
+    expect(out).to.have.property('on').and.to.be.a('function');
+    expect(out).to.have.property('emit').and.to.be.a('function');
+  });
+
+  it('wraps a stream that is piped in');
+
+  it('throws an error if a non-stream is piped in');
+
+  it('passes through events that are emitted on the stream');
+
+  it('passes through events that are emitted on a stream that is piped in');
+});
