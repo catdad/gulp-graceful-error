662d5fc089389935e6e16459465e353d39bbba96
Kiril Vatev <vatev.1@gmail.com>
2018-06-22 17:52:54 -0400

    using mock-stdio module for testing

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,11 @@ var stream = require('stream');
 var expect = require('chai').expect;
 var through = require('through2');
 var unstyle = require('unstyle');
+var fakeIo = require('mock-stdio');
+
+// fake some methods previously expected, because lazy
+fakeIo.activate = fakeIo.start;
+fakeIo.deactivate = fakeIo.end;
 
 // Only run tests entirely thorugh the vm when
 // running the npm coverage script. Otherwise,
@@ -37,44 +42,6 @@ Object.defineProperty(mod, 'lib', {
     return require(mod.filename);
   }
 });
-
-// allow hijacking IO, so that we can both
-// test values and not log during a test
-var fakeIo = (function () {
-
-  var originalStdout = process.stdout.write;
-  var originalStderr = process.stderr.write;
-
-  var outData = [];
-  var errData = [];
-
-  function collect(arr) {
-    return function (val) {
-      arr.push(new Buffer(val));
-    };
-  }
-
-  return {
-    activate: function () {
-      process.stdout.write = collect(outData);
-      process.stderr.write = collect(errData);
-    },
-    deactivate: function () {
-      process.stdout.write = originalStdout;
-      process.stderr.write = originalStderr;
-
-      var data = {
-        stdout: Buffer.concat(outData).toString(),
-        stderr: Buffer.concat(errData).toString()
-      };
-
-      outData = [];
-      errData = [];
-
-      return data;
-    }
-  };
-})();
 
 function getLibInVm(proc) {
   var start = '(function (exports, require, module, process) {';
